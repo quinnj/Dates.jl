68bda5b2a0a679cf7cbac2e24e9207128116dc28
Jacob Quinn <quinn.jacobd@gmail.com>
2014-04-21 14:10:08 -0400

    Move ratadie code to accessors.jl file

--- src/accessors.jl
+++ src/accessors.jl
@@ -1,3 +1,33 @@
+# Convert # of Rata Die days to proleptic Gregorian calendar y,m,d,w
+# Reference: http://mysite.verizon.net/aesir_research/date/date0.htm
+function _day2date(days)
+    z = days + 306; h = 100z - 25; a = fld(h,3652425); b = a - fld(a,4);
+    y = fld(100b+h,36525); c = b + z - 365y - fld(y,4); m = div(5c+456,153);
+    d = c - div(153m-457,5); return m > 12 ? (y+1,m-12,d) : (y,m,d)
+end
+function _year(days)
+   z = days + 306; h = 100z - 25; a = fld(h,3652425); b = a - fld(a,4);
+   y = fld(100b+h,36525); c = b + z - 365y - fld(y,4); m = div(5c+456,153); 
+   return m > 12 ? y+1 : y
+end 
+function _month(days)
+    z = days + 306; h = 100z - 25; a = fld(h,3652425); b = a - fld(a,4);
+    y = fld(100b+h,36525); c = b + z - 365y - fld(y,4); m = div(5c+456,153); 
+    return m > 12 ? m-12 : m
+end
+function _day(days)
+    z = days + 306; h = 100z - 25; a = fld(h,3652425); b = a - fld(a,4);
+    y = fld(100b+h,36525); c = b + z - 365y - fld(y,4); m = div(5c+456,153); 
+    return c - div(153m-457,5)
+end
+# https://en.wikipedia.org/wiki/Talk:ISO_week_date#Algorithms
+function _week(days)
+    w = div(abs(days-1),7) % 20871
+    c,w = divrem((w + (w >= 10435)),5218)
+    w = (w*28+[15,23,3,11][c+1]) % 1461
+    return div(w,28) + 1
+end
+
 # Accessor functions
 value(dt::Date) = dt.instant.days
 value(dt::UTDateTime) = dt.instant.t.ms
@@ -47,7 +77,7 @@ typemin{T<:DateTime}(::Type{T}) = DateTime(-292277023,1,1,0,0,0)
 typemax(::Type{Date}) = Date(252522163911149,12,31)
 typemin(::Type{Date}) = Date(-252522163911150,1,1)
 
-# TODO: optimize this?
+# TODO: optimize this
 function string(dt::DateTime)
     y,m,d = _day2date(_days(dt))
     h,mi,s = hour(dt),minute(dt),second(dt)
