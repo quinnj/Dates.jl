9b39fd3ae9fb6e2c67938bc5fe92feb2d0db9eec
Jacob Quinn <quinn.jacobd@gmail.com>
2014-05-27 23:19:01 -0400

    Update travis file

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,37 +1,30 @@
 using Dates
 using Base.Test
 
-include("test_types.jl")
-include("test_periods.jl")
-include("test_accessors.jl")
-include("test_query.jl")
-include("test_arithmetic.jl")
-include("test_conversions.jl")
-include("test_ranges.jl")
-include("test_adjusters.jl")
-include("test_io.jl")
-
-# Adjusters
+include("test/test_types.jl")
+include("test/test_periods.jl")
+include("test/test_accessors.jl")
+include("test/test_query.jl")
+include("test/test_arithmetic.jl")
+include("test/test_conversions.jl")
+include("test/test_ranges.jl")
+include("test/test_adjusters.jl")
+include("test/test_io.jl")
 
 
 #TODO
+ #formatting/parsing
+ #test coverage/coveralls.io
+ #docs update
 
-#d"2014-01-01"
-#dt"2014-01-01"
-
- #massage recur
+#NEED TESTS
+ 
+#IDEAS
  #research JSR-310, PHP? javascript? go? C#? for API completeness
  #round(dt,period)
- #add(dt,y,m,d,h,mi,s,ms); sub(dt,y,m,d,h,mi,s,ms); many adjusts at once?
- 
- #pair with recur
-  #tonext(dt,dayofweek); tonext(func,dt)
-  #toprev(dt,dayofweek); toprev(func,dt)
-  #tolast(dt,dayofweek,precision=Month); tolast(func,dt,precision=Month)
-  #tofirst(dt,dayofweek,precision=Month); tofirst(func,dt,precision=Month)
- #formatting/parsing
+ #add(dt,y,m,d,h,mi,s,ms); sub(dt,y,m,d,h,mi,s,ms); many ariths at once?
  #conversions: ruby? python? javascript? etc.
  #make TimeStamp fully parameterized (Instant, TimeZone, Calendar)
   #have datetime field + nanosecond field?
-#NEED TESTS
- 
+ #d"2014-01-01"
+ #dt"2014-01-01"
