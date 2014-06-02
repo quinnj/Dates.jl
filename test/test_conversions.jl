2fabf3854c2bac45b16d75c5ae87682b25e5252f
Jacob Quinn <quinn.jacobd@gmail.com>
2014-06-02 12:51:52 -0400

    Cleanup the conversion API. Now we have x2date, x2datetime and date2x, datetime2x for unix, rata, and julian. Also update tests.

--- test/test_conversions.jl
+++ test/test_conversions.jl
@@ -1,12 +1,12 @@
 # Test conversion to and from unix
-@test Dates.unix2datetime(Dates.date2unix(DateTime(2000,1,1))) == DateTime(2000,1,1)
+@test Dates.unix2datetime(Dates.datetime2unix(DateTime(2000,1,1))) == DateTime(2000,1,1)
 @test Dates.value(Dates.DateTime(1970)) == Dates.UNIXEPOCH
 # This test is very sensitive (second-precision)
 # so we need to warmup (i.e. compile) both sides of our equality
 # before running to ensure it passes
-Dates.date2unix(now())
+Dates.datetime2unix(now())
 time()
-@test Dates.date2unix(now()) == time()
+@test Dates.datetime2unix(now()) == time()
 
 # Tests from here: http://en.wikipedia.org/wiki/Unix_time
 @test string(Dates.unix2datetime(1095379198.75)) == string("2004-09-16T23:59:58.75Z")
@@ -32,17 +32,17 @@ time()
 @test string(Dates.unix2datetime(915148801.00)) == string("1999-01-01T00:00:01Z")
 @test string(Dates.unix2datetime(915148801.25)) == string("1999-01-01T00:00:01.25Z")
 
-@test Date(Dates.ratadays2date(734869)...) == Date(2013,1,1)
-@test Dates.date2ratadays(Date(Dates.ratadays2date(734869)...)) == 734869
+@test Date(Dates.rata2datetime(734869)) == Date(2013,1,1)
+@test Dates.date2rata(Dates.Date(Dates.rata2datetime(734869))) == 734869
 
 # Tests from here: http://mysite.verizon.net/aesir_research/date/back.htm#JDN
-@test Dates.julian2date(1721119.5) == DateTime(0,3,1)
-@test Dates.julian2date(1721424.5) == DateTime(0,12,31)
-@test Dates.julian2date(1721425.5) == DateTime(1,1,1)
-@test Dates.julian2date(2299149.5) == DateTime(1582,10,4)
-@test Dates.julian2date(2415020.5) == DateTime(1900,1,1)
-@test Dates.julian2date(2415385.5) == DateTime(1901,1,1)
-@test Dates.julian2date(2440587.5) == DateTime(1970,1,1)
-@test Dates.julian2date(2444239.5) == DateTime(1980,1,1)
-@test Dates.julian2date(2452695.625) == DateTime(2003,2,25,3)
-@test Dates.date2julian(DateTime(2013,12,3,21)) == 2456630.375
+@test Dates.julian2datetime(1721119.5) == DateTime(0,3,1)
+@test Dates.julian2datetime(1721424.5) == DateTime(0,12,31)
+@test Dates.julian2datetime(1721425.5) == DateTime(1,1,1)
+@test Dates.julian2datetime(2299149.5) == DateTime(1582,10,4)
+@test Dates.julian2datetime(2415020.5) == DateTime(1900,1,1)
+@test Dates.julian2datetime(2415385.5) == DateTime(1901,1,1)
+@test Dates.julian2datetime(2440587.5) == DateTime(1970,1,1)
+@test Dates.julian2datetime(2444239.5) == DateTime(1980,1,1)
+@test Dates.julian2datetime(2452695.625) == DateTime(2003,2,25,3)
+@test Dates.datetime2julian(DateTime(2013,12,3,21)) == 2456630.375
