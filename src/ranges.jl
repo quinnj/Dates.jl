b05a2c2b4b9cdfcd1211d8ff4c18409ec6d872d1
Jacob Quinn <quinn.jacobd@gmail.com>
2014-08-08 06:20:30 -0400

    Big ranges overhaul. Pretty much a complete rewrite here. ranges.jl file is now half the size it was and considerably less hacky. There's now just a clear, core functionality for determining the length of a date range that's used consistently in length(), steprem(), and in(). Major upgrade of range tests too.

--- src/ranges.jl
+++ src/ranges.jl
@@ -4,55 +4,28 @@
 Base.colon{T<:DateTime}(start::T, stop::T) = StepRange(start, Day(1), stop)
 
 # Given a start and end date, how many steps/periods are in between
-Base.length(r::StepRange{Date,Day}) = isempty(r) ? 0 : 
-    int(div(r.stop + r.step - r.start, r.step))
-Base.length(r::StepRange{Date,Week}) = isempty(r) ? 0 : 
-    int(div(r.stop + r.step - r.start, 7*value(r.step)))
-Base.length{T<:FixedPeriod}(r::StepRange{DateTime,T}) = isempty(r) ? 0 : 
-    int(div(r.stop + r.step - r.start, toms(r.step)))
-
-# Calculate a conservative guess for how many months/years are between two dates
-guess(start::Date,stop::Date,step::Month) = int(float(stop-start)/(30.436875*value(step)))
-guess(start::Date,stop::Date,step::Year) = int(float(stop-start)/(365.2425*value(step)))
-guess(start::DateTime,stop::DateTime,step::Month) = int(float(days(stop-start))/(30.436875*value(step)))
-guess(start::DateTime,stop::DateTime,step::Year) = int(float(days(stop-start))/(365.2425*value(step)))
-
-function _length(start,stop,step)
-    start,stop = start > stop ? (stop,start) : (start,stop)
-    step = step < zero(step) ? -step : step
-    i = guess(start,stop,step)
-    while (start+step*i) <= stop
+guess(a::DateTime,b::DateTime,c) = ifloor((int128(b) - int128(a))/toms(c))
+guess(a::Date,b::Date,c) = int(div(int(b - a),days(c)))
+function len(a,b,c)
+    lo, hi, st = min(a,b), max(a,b), abs(c)
+    i = guess(a,b,c)-1
+    while lo+st*i <= hi
         i += 1
     end
-    return i
+    return i-1
 end
-
-Base.length{T<:TimeType}(r::StepRange{T}) = isempty(r) ? 0 : _length(r.start,r.stop,r.step)
+Base.length{T<:TimeType}(r::StepRange{T}) = isempty(r) ? 0 : len(r.start,r.stop,r.step) + 1
 # Period ranges hook into Int64 overflow detection
 Base.length{P<:Period}(r::StepRange{P}) = length(StepRange(value(r.start),value(r.step),value(r.stop)))
 
 # Used to calculate the last valid date in the range given the start, stop, and step
 # last = stop - steprem(start,stop,step)
-Base.steprem(a::Date,b::Date,c::Day) = (b-a) % c
-Base.steprem(a::Date,b::Date,c::Week) = (b-a) % (7*value(c))
-Base.steprem(a::DateTime,b::DateTime,c::FixedPeriod) = (b-a) % toms(c)
-Base.steprem{T<:TimeType}(a::T,b::T,step::Period) = b - (a+step*(_length(a,b,step)-1))
+Base.steprem{T<:TimeType}(a::T,b::T,c) = b - (a + c*len(a,b,c))
 
 import Base.in
-function in{T<:TimeType,S<:Period}(x::T, r::StepRange{T,S})
-    isempty(r) && return false
-    if step(r) < zero(S)
-        for d in r
-            d == x && return true
-            x > d && break
-        end
-    else
-        for d in r
-            d == x && return true
-            x < d && break
-        end
-    end
-    return false
+function in{T<:TimeType}(x::T, r::StepRange{T})
+    n = len(first(r),x,step(r)) + 1
+    n >= 1 && n <= length(r) && r[n] == x
 end
 
 Base.start{T<:TimeType}(r::StepRange{T}) = 0
